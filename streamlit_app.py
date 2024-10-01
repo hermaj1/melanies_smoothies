f00d6d8feba6c301336b220639143aa1da07fb49
Jochen Hermanns <jochen.hermanns@msg.group>
2024-10-01 10:21:35 +0000

    use pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -18,8 +19,13 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+# convert dataframe to pandas dataframe
+pd_df = my_dataframe.to_pandas
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
@@ -34,7 +40,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        # new sction to display fruitvice nutriton information
+        # new section to display fruitvice nutriton information
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
@@ -43,8 +50,6 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop
     time_to_insert = st.button('Submit Order')    
     
     if time_to_insert:
